96f8291ade74eec174f71b38ce374bc6f196bf99
Zaroori Return <www.hackertechyt@gmail.com>
2024-10-27 03:59:39 -0700

    Update send-notification.js

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.zaroorireturn.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.dadearn.online');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
